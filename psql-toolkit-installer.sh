ea8b43090e5ee3bbd65f412375bdb2114155ee91
Jesús Rafael Sánchez <jesus@neotec.do>
2021-06-12 02:58:03 -0400

    Fixing menu spacing

---
+++ psql-toolkit-installer.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+#
+# psql-toolkit-installer.sh
+# Copyright (C) 2021 Jesus R. Sanchez <jesus@neotec.do>
+#
+# Distributed under terms of the BSD license.
+#
+
+splash() {
+    echo "                #                                            # #    #"
+    echo "                #   ####   ####     #      #####             # #    #"
+    echo "                #    #  #   #  #   ###     # # #             # #      #"
+    echo "####   ##   ### #    #   #  # #    # #       #    ###   ###  # #  # # ##"
+    echo "#   # # # ##  # #    #   #  ###   #  #       #   #   # #   # # # #  # #"
+    echo "#   #  #  #   # #    #   #  #  #  #####      #   #   # #   # # ###  # #"
+    echo "#  ## # # #   # #    #  #   #  #  #   #      #   #   # #   # # #  # # #"
+    echo "###   ##   #### #   ####   ####  ##   ##    ###   ###   ###  # ## # # ##"
+    echo "#             #"
+    echo "##           ##"
+    return 0
+}
+
+install_process() {
+    mkdir -p ${HOME}/.config/psql
+    cp -frv .config/psql/* ${HOME}/.config/psql
+    cp -frv .psqlrc ${HOME}/
+    return $?
+}
+
+
+
+if [ ! -d ${HOME}/.config/psql ];
+then
+    read -n 1 -s -r -p "Press any key to continue the install process..."
+    install_process
+    RETVAL=$?
+else
+    echo "psql DBA Toolkit already installed..."
+    echo "creating backup to reinstall"
+    mv -fv ${HOME}/.config/psql ${HOME}/.config/psql_backup
+    mv -fv ${HOME}/.psqlrc ${HOME}/.psqlrc_backup
+
+    read -n 1 -s -r -p "Press any key to continue the install process..."
+    install_process
+    RETVAL=$?
+fi
+
+exit ${RETVAL}
+
+
